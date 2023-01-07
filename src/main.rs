4f2d1a22030929292562b09d7968c05064f66930
seisuke <seisuke@ymail.com>
2023-01-07 22:12:08 +0900

    uncomment out

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
